9e8f36bcace2a375ca73acc96b1c172fe8a6dde9
ErwinKomen <E.Komen@ru.nl>
2022-10-03 12:22:51 +0200

    apps.py

--- lila/lila/lict/apps.py
+++ lila/lila/lict/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class lictConfig(AppConfig):
-    name = 'lict'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lila.lict'
--- lila/lila/reader/apps.py
+++ lila/lila/reader/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class readerConfig(AppConfig):
-    name = 'reader'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lila.reader'
